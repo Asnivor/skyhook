5e346747a2bcfa4f9e8b449ec99264d970f3a1fe
Asnivor <coding@asnitech.co.uk>
2017-09-13 15:59:49 +0100

    Modified title to show buildVersion rather than buildNumber and also the commit message

--- providers/appveyor.js
+++ providers/appveyor.js
@@ -11,7 +11,7 @@ class AppVeyor extends BaseProvider {
     async parseData() {
         this.payload.setEmbedColor(0xFFFFFF);
         this.payload.addEmbed({
-            title: "Build #" + this.body.eventData.buildNumber,
+            title: "Build #" + this.body.eventData.buildVersion + " - " + this.body.eventData.commitMessage,
             url: this.body.eventData.buildUrl,
             author: {
                 name: this.body.eventData.projectName
